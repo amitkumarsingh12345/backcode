cffcaf261274c637d22a5a301e4561dbf4164b40
amitkumarsingh12345 <163759129+amitkumarsingh12345@users.noreply.github.com>
2024-07-25 18:33:32 +0530

    Update Cards.js

--- frontcode/src/Users/Caponents/Cards.js
+++ frontcode/src/Users/Caponents/Cards.js
@@ -6,7 +6,7 @@ function Cards({ props }) {
   return (
     <Card className='w-100 w-xs-75 w-sm-50' style={{ width: '13rem' }}>
       <Nav.Link href={`/AddToCart/${JSON.stringify(props)}`} className="p-3">
-         <Card.Img variant="top" src={props.image} alt="" style={{maxHeight: '180px'}}/>
+         <Card.Img variant="top" src={props.image} alt="" style={{maxHeight: '150px'}}/>
       </Nav.Link>
       <Card.Body>
           <Acordian data={props}/>
